d9fdbcdd679fcc258242bcdd67cfc80a5c2e48a4
alex100dev@gmail.com <alex100dev@gmail.com>
2022-06-29 07:19:38 +0600

    Make dist

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+
+const func = {
+  readFileSync: function(path) {
+    var encoding;
+    if (path.split('.')[1] == 'html') {
+      encoding = 'utf8';
+    } 
+    return fs.readFileSync(path, {
+      encoding: encoding, 
+      flag: 'r'
+    });
+  }
+};
 
 const HOST = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
- console.log(request.method);
- response.end('hello');
+  if (request.method == 'GET') {
+    if (request.url == '/') {
+      response.setHeader("Content-Type", "text/html");
+      response.end(func.readFileSync(__dirname + '/dist/index.html'));
+    }
+    
+  } 
 });
 server.listen(PORT, HOST, () => {
   console.log(`Server on ${HOST}:${PORT}`);
